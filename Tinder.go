886cf9102ed488e3f5d818e7a30af81c98f99abf
taner <taner.ozel@primeapps.io>
2020-11-23 21:37:40 +0300

    add login with sms

--- Tinder.go
+++ Tinder.go
@@ -15,8 +15,36 @@ func NewTinderClient(token string) *client {
 	}
 }
 
-func (t *client) SetHeader(key string, value string) {
-	t.requester.SetHeader(key, value)
+func LoginSms(phone string) {
+	r := NewRequest()
+	var headerJson = []byte(`{
+    "accept": "application/json",
+    "accept-language": "tr,en;q=0.9,en-GB;q=0.8,en-US;q=0.7",
+    "app-session-id": "2b5351ba-2bec-49b7-be55-aeaff9b76ea1",
+    "app-session-time-elapsed": "-3508674",
+    "app-version": "1026300",
+    "content-type": "application/x-google-protobuf",
+    "funnel-session-id": "73ad6deae5a0b3f6",
+    "persistent-device-id": "7e6e7592-d428-4109-9fdc-f022513b7897",
+    "platform": "web",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "cross-site",
+    "tinder-version": "2.63.0",
+    "user-session-id": "null",
+    "user-session-time-elapsed": "24159",
+    "x-supported-image-formats": "jpeg"
+  }`)
+	c := make(map[string]json.RawMessage)
+
+	// unmarschal JSON
+	json.Unmarshal(headerJson, &c)
+
+	r.Post("https://api.gotinder.com/v3/auth/login?locale=tr").Send("\n\u000e\n\f" + phone)
+	for k, v := range c {
+		r.Set(k, string(v))
+	}
+	r.End()
 }
 
 func (t *client) RecsCore() ([]RecsCoreUser, error) {
@@ -40,23 +68,23 @@ func (t *client) RecsCore() ([]RecsCoreUser, error) {
 	return recs.Data.Results, nil
 }
 
-func (t *client) LikeUser(userId string) ([]RecsCoreUser, error) {
+func (t *client) LikeUser(userId string) (string, error) {
 	var recs RecsCoreResponse
 
 	url := "like/" + userId
 	b, errs := t.requester.Post(url, "")
 	if errs != nil {
-		return recs.Data.Results, errs[0]
+		return b, errs[0]
 	}
 
 	err := json.Unmarshal([]byte(b), &recs)
 	if err != nil {
-		return recs.Data.Results, err
+		return b, err
 	}
 
-	if recs.Meta.Status != 200 {
-		return recs.Data.Results, errors.New("Error getting Recs Core")
-	}
+	return b, nil
+}
 
-	return recs.Data.Results, nil
+func (t *client) loginAuth(phone string) {
+	t.requester.Post("auth/login", phone)
 }
--- example/TinderApp.go
+++ example/TinderApp.go
@@ -3,15 +3,29 @@ package main
 import (
 	Tinder "TinderClient"
 	"fmt"
+	"time"
 )
 
 func main() {
-	TinderApp := Tinder.NewTinderClient("9a8a541d-cb08-4a14-8151-25e36d3253ca")
-	TinderApp.SetHeader("aa", "aa")
+	//Tinder.LoginSms("905380310832")
+	TinderApp := Tinder.NewTinderClient("f203f979-636b-42d8-b8d8-61d407d6c7ce")
+
 	data, err := TinderApp.RecsCore()
-	if err == nil {
+
+	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(data)
+
+	for i := 0; i < len(data); i++ {
+		item := data[i]
+		userId := item.User.ID
+
+		go func() {
+			go TinderApp.LikeUser(userId)
+			go fmt.Println("Like : "+userId, item.User.Name)
+		}()
+		time.Sleep(time.Second)
+	}
+	fmt.Scanln("")
 
 }
